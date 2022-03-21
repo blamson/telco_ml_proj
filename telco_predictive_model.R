4fd847c76a3dce1f4fc92db066b09ac758a2b080
Brady Lamson <lamsonbrady@gmail.com>
2022-03-21 15:56:20 -0600

    initial testing

--- telco_predictive_model.R
+++ telco_predictive_model.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(tidymodels)
 library(yardstick)
+library(rsample)
+library(parsnip)
 
 # Source of Guides --------------------------------------------------------------------------------
 # This methodology strictly follows the advice provided in Chapter 10 of Modern R for Data Science
@@ -12,8 +14,14 @@ library(yardstick)
 # Prepping Data -----------------------------------------------------------------------------------
 
 # Read in data
+# Convert majority of columns to factor for ease of use
 telco_data <- readr::read_csv("data/WA_Fn-UseC_-Telco-Customer-Churn.csv") %>%
-    dplyr::mutate(Churn = as.factor(Churn))
+    dplyr::mutate(
+        dplyr::across(
+            where(is.character), as.factor
+        ),
+        customerID = customerID %>% as.character()
+    )
 
 # Set consistent seed
 set.seed(364)
@@ -44,14 +52,48 @@ pi_bar <- train %>%
 
 glue::glue("The estimated churn rate of the training sample is approximately {pi_bar %>% round(digits=3)}")
 
-
+# Null Model ---------------------------------------------------
 # Create the null model
 mod_null <- logistic_reg(mode = "classification") %>%
     set_engine("glm") %>%
     fit(Churn ~ 1, data = train)
 
-# Create linear model
+# Compute accuracy of null model for future comparisons
+pred <- train %>%
+    dplyr::bind_cols(
+        predict(mod_null, new_data = train, type = "class")
+    ) %>% 
+    dplyr::rename(Churn_null = .pred_class)
 
+# Method 1 ----
+yardstick::accuracy(data = pred, Churn, Churn_null)
+
+# Method 2 ----
+confusion_null <- pred %>%
+    yardstick::conf_mat(truth = Churn, estimate = Churn_null)
+confusion_null
+
+# Linear Model -------------------------------------------------
+
+# Remove customerID from train dataset
+train_no_id <- train %>%
+    dplyr::select(-customerID)
+
+# Create linear model using all columns as predictors
 mod_log <- logistic_reg(mode = "classification") %>%
     set_engine("glm") %>%
-    fit(Churn ~ ., data = train)
+    fit(Churn ~ ., data = train_no_id)
+
+# Compute accuracy of model ----
+pred <- pred %>%
+    dplyr::bind_cols(
+        predict(mod_log, new_data = train, type = "class")
+    ) %>%
+    dplyr::rename(Churn_log = .pred_class)
+
+confusion_log <- pred %>%
+    yardstick::conf_mat(truth = Churn, estimate = Churn_log)
+
+confusion_log
+
+yardstick::accuracy(data = pred, Churn, Churn_log)
